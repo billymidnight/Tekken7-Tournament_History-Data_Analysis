623e5980d12b192e49e539ff18cd67f22a5c695c
billymidnight <139000708+billymidnight@users.noreply.github.com>
2023-08-17 16:27:52 +0000

    Add files via upload

---
+++ WinPercent.sql
@@ -0,0 +1,18 @@
+SELECT
+  p.name AS participant,
+  COUNT(*) FILTER (WHERE c.winner = p.name) AS wins,
+  COUNT(DISTINCT CASE WHEN c.home = p.name THEN c.year || c.round ELSE NULL END) +
+    COUNT(DISTINCT CASE WHEN c.away = p.name THEN c.year || c.round ELSE NULL END) AS appearances,
+  ROUND(COALESCE(COUNT(*) FILTER (WHERE c.winner = p.name)::numeric / 
+    NULLIF(COUNT(DISTINCT CASE WHEN c.home = p.name THEN c.year || c.round ELSE NULL END) +
+    COUNT(DISTINCT CASE WHEN c.away = p.name THEN c.year || c.round ELSE NULL END), 0) * 100, 0), 2) AS win_percentage
+FROM
+  participants_table AS p
+JOIN
+  tournament_history AS c ON p.name = c.winner OR p.name = c.home OR p.name = c.away
+--WHERE c.round in ('Final') 
+GROUP BY
+  p.name
+ORDER BY
+  p.name;
+  

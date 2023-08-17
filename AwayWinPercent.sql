8b4355c333199730456ae461419fc38724bc788a
billymidnight <139000708+billymidnight@users.noreply.github.com>
2023-08-17 15:53:54 +0000

    Update AwayWinPercent.sql

--- AwayWinPercent.sql
+++ AwayWinPercent.sql
@@ -1,11 +1,15 @@
+-- Calculates the win percentage for each participant whilst playing away from home
+-- Query outputs away wins, away appearances and then away_wins/away_appearances*100 i.e. away win percentage alongside each name from participant_table
+--Inner Join utilized
+
 SELECT
   p.name AS participant,
   COUNT(*) FILTER (WHERE c.away = p.name AND c.winner = p.name) AS away_wins,
   COUNT(*) FILTER (WHERE c.away = p.name) AS away_appearances,
  (COUNT(*) FILTER (WHERE c.away = p.name AND c.winner = p.name) / COUNT(*) FILTER (WHERE c.away = p.name)::float) * 100 AS away_win_percentage
- FROM participants AS p
+ FROM participants_table AS p
 JOIN
-centralunit AS c ON p.name = c.away
+tournament_history AS c ON p.name = c.away
 
 GROUP BY
 p.name

4cf7fb81f2085c7d5037b06042dd4c036941bc64
billymidnight <139000708+billymidnight@users.noreply.github.com>
2023-08-20 20:21:39 +0000

    Update Nation_Trophies.sql

--- Nation_Trophies.sql
+++ Nation_Trophies.sql
@@ -1,9 +1,11 @@
+--Groups participants by nation and aggregates trophies for nation for nationality-based analysis
+--Group by clause and inner join utilized
 SELECT p.nation AS nation,
        SUM(p.trophies) AS total_trophies
-FROM participants AS p
+FROM participants_table AS p
 JOIN (
     SELECT winner, COUNT(*) AS trophies
-    FROM centralunit
+    FROM tournament_history
     WHERE round = 'Final'
     GROUP BY winner
 ) AS c ON p.name = c.winner
